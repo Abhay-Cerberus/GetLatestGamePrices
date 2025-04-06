b19c074ef1909bf71f1394c2e312138a0260d09a
Abhay-Cerberus <94872391+Abhay-Cerberus@users.noreply.github.com>
2025-04-06 21:40:33 +0530

    modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="GetLatestGamePrices",           # Replace with your package name
-    version="0.2.0",            # Set your version
+    name="GetLatestGamePrices",           
+    version="0.2.0",            
     description="This Package gets you the deals about a single game from ggdeals. also this is my first package to learn about things like packages, version control, github actions",
     author="AbhayCerberus",
     author_email="mistercerberus01+pypiPackage@gmail.com",
-    packages=find_packages(),   # Automatically find all packages (like my_package)
+    packages=find_packages(),   
     install_requires=[
         "requests",
         "beautifulsoup4"
